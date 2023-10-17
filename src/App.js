5badd8455cfae286aa11e5ca7476b4009bcaca3a
sri <131853932+sripriyadharsh@users.noreply.github.com>
2023-10-17 22:50:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App=()=>{
+  const [email, setEmail]=useState('');
+  const[isValidEmail, setIsValidEmail]=useState(true);
+  
+  const [name,setName]=useState('');
+  const [isValidName, setIsValidName]=useState(true);
+
+const handleNameChange=(e)=>{
+  setName(e.target.value);
+  setIsValidName(e.target.value?.length > 0)
+}
+const handleEmailChange=(e)=>{
+  const inputEmail=e.target.value;
+  const emailPattern=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  setIsValidName(emailPattern.test(inputEmail));
+  setEmail(inputEmail);
+  
+}
+const handleSubmit=(e)=>{
+  e.preventDefault();
+
+  const emailPattern=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  setIsValidEmail(emailPattern.test(email))
+
+  setIsValidName(name.length >0 )
+  isValidEmail && isValidName ? alert('Form submitted'):
+  alert('Form validation failed');
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Form validation</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='email'>Email   </label>
+          <input
+          type='text'
+          value={email}
+          id='email'
+          onChange={handleEmailChange}
+          className={isValidEmail? 'valid' : 'invalid'}>
+            
+          </input>
+        </div>
+        <div className='form-feedback'>
+          {
+            !isValidEmail && (<span className='invalid-email'>Enter valid email</span>)
+          }
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='name'>Name   </label>
+          <input
+          type='text'
+          value={name}
+          id='name'
+          onChange={handleNameChange}
+          className={isValidName? 'valid' : 'invalid'}>
+            
+          </input>
+        </div>
+        <div className='form-feedback'>
+          {
+            !isValidName && (<span className='invalid-name'>Enter valid name</span>)
+          }
+        </div>
+          <br></br>
+        <button type='submit'>submit</button>
+      </form>
     </div>
   );
+
 }
 
 export default App;
